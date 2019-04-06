20a8ca3ccb0a27502e7c518abbd2e4da7c778a9b
André <andre.carvalho1985@gmail.com>
2019-04-06 18:19:14 -0300

    add angular-bootstrap and first componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IndexComponent } from './content/index/index.component';
+import { InfoComponent } from './content/info/info.component';
+import { LoginComponent } from './content/login/login.component';
+import { StockWatchComponent } from './content/stock-watch/stock-watch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IndexComponent,
+    InfoComponent,
+    LoginComponent,
+    StockWatchComponent,
+    NgbModule
   ],
   imports: [
     BrowserModule
---
+++ src/app/content/stock-watch/stock-watch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-watch',
+  templateUrl: './stock-watch.component.html',
+  styleUrls: ['./stock-watch.component.scss']
+})
+export class StockWatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
